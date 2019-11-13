c85b98d96410f384ac96a2c80b2cf0918196a73b
Paymon <paymon@geeks.ltd.uk>
2019-11-13 17:53:28 +0000

    no message

--- Android/ScheduledAlarmHandler.cs
+++ Android/ScheduledAlarmHandler.cs
@@ -1,8 +1,6 @@
 ﻿namespace Zebble.Device
 {
-    using Android.App;
     using Android.Content;
-    using Android.OS;
     using Newtonsoft.Json;
     using System;
 
@@ -26,26 +24,6 @@
             {
                 Log.Error(ex);
             }
-        }
-    }
-
-    [Service(Enabled = true)]
-    public class ScheduledAlarmService : Service
-    {
-        public override IBinder OnBind(Intent intent) => null;
-
-        public override async void OnCreate()
-        {
-            base.OnCreate();
-
-            var intentFilter = await LocalNotification.SetActionFilters();
-            RegisterReceiver(LocalNotification.ReceiverInstance, intentFilter);
-        }
-
-        public override void OnDestroy()
-        {
-            base.OnDestroy();
-            if (LocalNotification.ReceiverInstance != null) UnregisterReceiver(LocalNotification.ReceiverInstance);
         }
     }
 }
---
+++ Android/ScheduledAlarmService.cs
@@ -0,0 +1,28 @@
+﻿namespace Zebble.Device
+{
+    using Android.App;
+    using Android.Content;
+    using Android.OS;
+    using Android.Runtime;
+
+    [Service(Enabled = true)]
+    [Preserve]
+    public class ScheduledAlarmService : Service
+    {
+        public override IBinder OnBind(Intent intent) => null;
+
+        public override async void OnCreate()
+        {
+            base.OnCreate();
+
+            var intentFilter = await LocalNotification.SetActionFilters();
+            RegisterReceiver(LocalNotification.ReceiverInstance, intentFilter);
+        }
+
+        public override void OnDestroy()
+        {
+            base.OnDestroy();
+            if (LocalNotification.ReceiverInstance != null) UnregisterReceiver(LocalNotification.ReceiverInstance);
+        }
+    }
+}
