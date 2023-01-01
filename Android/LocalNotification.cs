a2fe312d68ff5f34d458f07c5ecec0b420c11587
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2023-01-01 14:38:56 +0330

    Fix a linking issue

--- Android/LocalNotification.cs
+++ Android/LocalNotification.cs
@@ -3,6 +3,7 @@
     using Android.App;
     using Android.Content;
     using Android.OS;
+    using Android.Runtime;
     using Java.Lang;
     using Newtonsoft.Json;
     using Olive;
@@ -10,6 +11,7 @@
     using System.Collections.Generic;
     using System.Threading.Tasks;
 
+    [Preserve]
     public static partial class LocalNotification
     {
         static PowerManager GetPowerManager(Android.Content.Context context) => PowerManager.FromContext(context);
